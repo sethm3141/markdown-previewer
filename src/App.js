57682afbae327edb7b9784e071332ddae82c98fb
sethm3141 <sethmaupin@gmail.com>
2024-01-12 13:53:35 -0600

    continued the project and added the text area

--- src/App.js
+++ src/App.js
@@ -9,11 +9,16 @@ function App() {
             <i className='fa-brands fa-free-code-camp'></i>
             <h3 className='ms-2 mb-1'>Editor</h3>
           </div>
-          <div className='right-side-header'>
+          <div className='right-side-header d-flex align-items-center'>
             <i className='fa-solid fa-maximize font-weight-light'></i>
           </div>
         </div>
-        <div className='body'></div>
+        <div className='body'>
+          <textarea id='editor' rows='4' cols='50'>
+            At w3schools.com you will learn how to make a website. They offer
+            free tutorials in all web development technologies.
+          </textarea>
+        </div>
       </section>
       <section id='previewer' className='card'>
         <div className='header d-flex justify-content-between'>
@@ -21,7 +26,7 @@ function App() {
             <i className='fa-brands fa-free-code-camp'></i>
             <h3 className='ms-2 mb-1'>Previewer</h3>
           </div>
-          <div className='right-side-header'>
+          <div className='right-side-header d-flex align-items-center'>
             <i className='fa-solid fa-maximize font-weight-light'></i>
           </div>
         </div>
